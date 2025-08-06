d88cb30041c840f4cc4cfee8d85fdab542964095
Md. Abdullah Al Fahad Bhuiyan <md.a.fahad25@gmail.com>
2025-08-06 23:40:05 +0600

    1.Added internet permission  2.Fixed wrong field in the response model

--- app/src/main/java/com/example/fahad/ifarmerott/data/model/Movie.kt
+++ app/src/main/java/com/example/fahad/ifarmerott/data/model/Movie.kt
@@ -3,6 +3,7 @@ package com.example.fahad.ifarmerott.data.model
 data class Movie(
     val Title: String,
     val Year: String,
+    val imdbID: String,
     val Type: String,
     val Poster: String
 )
--- app/src/main/java/com/example/fahad/ifarmerott/data/model/MovieResponse.kt
+++ app/src/main/java/com/example/fahad/ifarmerott/data/model/MovieResponse.kt
@@ -1,7 +1,7 @@
 package com.example.fahad.ifarmerott.data.model
 
 data class MovieResponse(
-    val movies: List<Movie>,
+    val Search: List<Movie>,
     val totalResults: String,
     val Response: String
 )
