5752a67e90b7583cc0d1a50e4bc28d0629c31611
Md. Abdullah Al Fahad Bhuiyan <md.a.fahad25@gmail.com>
2025-08-07 12:47:13 +0600

    Added getMovieDetails api in repository

--- app/src/main/java/com/example/fahad/ifarmerott/repository/MovieRepository.kt
+++ app/src/main/java/com/example/fahad/ifarmerott/repository/MovieRepository.kt
@@ -1,11 +1,16 @@
 package com.example.fahad.ifarmerott.repository
 
 import com.example.fahad.ifarmerott.data.api.RetrofitInstance
+import com.example.fahad.ifarmerott.data.model.Movie
 import com.example.fahad.ifarmerott.data.model.MovieResponse
 import com.example.fahad.ifarmerott.utils.Constants
 
 class MovieRepository {
     suspend fun searchMovies(query: String, page: Int = 1, year: String? = null): MovieResponse {
         return RetrofitInstance.api.searchMovies(Constants.API_KEY, query, page, year)
+    }
+
+    suspend fun getMovieDetails(imdbId: String): Movie? {
+        return RetrofitInstance.api.getMovieDetails(Constants.API_KEY, imdbId)
     }
 }
