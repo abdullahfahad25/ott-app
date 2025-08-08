33f391abdc48719f3153bc63bb4ba2572f0f7988
Md. Abdullah Al Fahad Bhuiyan <md.a.fahad25@gmail.com>
2025-08-08 23:04:49 +0600

    Added unit test for DetailsViewModel

---
+++ app/src/test/java/com/example/fahad/ifarmerott/details/viewmodel/DetailsViewModelTest.kt
@@ -0,0 +1,16 @@
+package com.example.fahad.ifarmerott.details.viewmodel
+
+import com.example.fahad.ifarmerott.common.data.model.Movie
+import com.example.fahad.ifarmerott.common.repository.MovieRepository
+import org.junit.Assert.*
+import org.junit.Test
+
+class DetailsViewModelTest {
+    private val repository = MovieRepository()
+    private val viewModel = DetailsViewModel(repository)
+
+    @Test
+    fun movieDetailsTest() {
+        assertEquals(null, viewModel.movieDetails.value)
+    }
+}
